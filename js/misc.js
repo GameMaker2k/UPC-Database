575f4b54f4708e374c2f88c300253241b76b4187
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2012-03-01 19:15:06 -0600

    Another JavaScript update.

--- js/misc.js
+++ js/misc.js
@@ -7,9 +7,12 @@
     alert("You need to enter a UPC/EAN!");
     return false; }
   kittyCode();
-  if(str.length!=8&&str.length!=12&&str.length!=13) {
+  if(str.length!=7&&str.length!=8&&str.length!=11&&
+	  str.length!=12&&str.length!=13) {
     alert("Invalid UPC/EAN!");
     return false; }
+  if(str.length==7) {
+    document.upcform.upc.value = fix_upce_checksum(str); }
   if(str.length==8&&!validate_upce(str,false)&&!validate_ean8(str,false)) {
     document.upcform.upc.value = fix_upce_checksum(str); }
   if(str.length==8&&validate_upce(str,false)) {
@@ -18,6 +21,8 @@
   if(str.length==8&&validate_ean8(str,false)) {
     document.upcform.upc.value = convert_ean8_to_ean13(str); 
 	str = document.upcform.upc.value; }
+  if(str.length==11) {
+    document.upcform.upc.value = fix_upca_checksum(str); }
   if(str.length==12&&!validate_upca(str,false)) {
     document.upcform.upc.value = fix_upca_checksum(str); }
   if(str.length==12&&validate_upca(str,false)) {
