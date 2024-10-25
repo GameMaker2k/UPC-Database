bbd425bc7157a36cbaf20fe4c484f29a1201ce86
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-10-25 13:25:02 -0500

    Updated Years to 2024 ^_^

--- js/validate.js
+++ js/validate.js
@@ -7,9 +7,9 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     Revised BSD License for more details.
 
-    Copyright 2011-2012 Cool Dude 2k - http://idb.berlios.de/
-    Copyright 2011-2012 Game Maker 2k - http://intdb.sourceforge.net/
-    Copyright 2011-2012 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
+    Copyright 2011-2024 Cool Dude 2k - http://idb.berlios.de/
+    Copyright 2011-2024 Game Maker 2k - http://intdb.sourceforge.net/
+    Copyright 2011-2024 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
     $FileInfo: validate.js - Last Update: 02/28/2012 Ver. 2.2.5 RC 1 - Author: cooldude2k $
 */
--- misc/adminupc.php
+++ misc/adminupc.php
@@ -8,9 +8,9 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     Revised BSD License for more details.
 
-    Copyright 2011-2012 Cool Dude 2k - http://idb.berlios.de/
-    Copyright 2011-2012 Game Maker 2k - http://intdb.sourceforge.net/
-    Copyright 2011-2012 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
+    Copyright 2011-2024 Cool Dude 2k - http://idb.berlios.de/
+    Copyright 2011-2024 Game Maker 2k - http://intdb.sourceforge.net/
+    Copyright 2011-2024 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
     $FileInfo: adminupc.php - Last Update: 10/24/2024 Ver. 2.2.5 RC 1 - Author: cooldude2k $
 */
